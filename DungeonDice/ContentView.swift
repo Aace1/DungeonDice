201fb27278161fb9456aca6d073f9a413e05a4ed
Aaron Anderson <aace2@me.com>
2023-03-30 11:31:57 -0700

    Dungeon Dice Chapter 4.6								Code Modularity w/Extracted Views and Extensions w/Computed Parties

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,16 +8,105 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    enum Dice: Int, CaseIterable {
+        case four = 4
+        case six = 6
+        case eight = 8
+        case ten = 10
+        case twelve = 12
+        case twenty = 20
+        case hundred = 100
+        
+        func roll() -> Int {
+            return Int.random(in: 1...self.rawValue)
         }
-        .padding()
+    }
+    
+    @State private var resultMessage = ""
+    @State private var buttonsLeftOver = 0 // # of buttons in a less-than-full row
+    
+    let horizontalPadding: CGFloat = 16
+    let spacing: CGFloat = 0 //betweem buttons
+    let buttonWidth: CGFloat = 102
+    
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                titleView
+                
+                Spacer()
+                
+                resultMessageView
+                
+                
+                Spacer()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: buttonWidth), spacing: spacing)]) {
+                    ForEach(Dice.allCases.dropLast(buttonsLeftOver), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice."
+                        }
+                        .frame(width: buttonWidth)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                }
+                
+                HStack {
+                    ForEach(Dice.allCases.suffix(buttonsLeftOver), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice."
+                        }
+                        .frame(width: buttonWidth)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                }
+                
+            }
+            .padding()
+            .onChange(of: geo.size.width, perform: { newValue in
+                arrangeGrideItems(deviceWidth: geo.size.width)
+            })
+            
+            .onAppear {
+                arrangeGrideItems(deviceWidth: geo.size.width)
+            }
+        }
+    }
+    
+    func arrangeGrideItems(deviceWidth: CGFloat) {
+        var screenWidth = deviceWidth - horizontalPadding * 2 //padding on both sides
+        if Dice.allCases.count > 1 {
+            screenWidth += spacing
+        }
+        
+        // calculate numOfButtonsPerRow as an Int
+        let numberOfuttonsPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+        buttonsLeftOver = Dice.allCases.count % numberOfuttonsPerRow
+        //print("numberOFbuttonsPerRow = \(numberOfuttonsPerRow)")
+        //print("buttonsLeftOver = \(buttonsLeftOver)")
     }
 }
+
+extension ContentView {
+    private var titleView: some View {
+        Text("Dungeon Dice")
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundColor(.red)
+    }
+    private var resultMessageView: some View {
+        Text(resultMessage)
+            .font(.largeTitle)
+            .fontWeight(.medium)
+            .multilineTextAlignment(.center)
+            .frame(height: 150)
+    }
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
